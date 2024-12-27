5674b5e89257c7da6f2b11c178fe76b0f5a58b62
sergey-smirnov-hach <sergey.smirnov636@gmail.com>
2024-12-27 19:31:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+name = ("urbay")
+print(2.0)
+print(type(2.0))
+print('Hello World')
+print(type(5 + 5))
+print(type('helo, world'))
+print(5 // 5)
+print(5 != 5 and 5 < 10)
+print(5 == 5 , 5 < 10)
+print(type(int('5')))
+
+print(name, type(name))
+name = 5
+
